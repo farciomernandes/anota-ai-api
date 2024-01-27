9191ebd8c84e0d6a426e6bef8a75119aaae04908
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-27 11:56:18 -0300

    chore: initial commit with husky lint staged and tests

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.ts',
+      '!<rootDir>/src/main/**',
+      '!<rootDir>/src/**/*-protocols.ts',
+      '!<rootDir>/**/protocols/**'
+    ],
+    coverageDirectory: "coverage",
+    testEnvironment: 'node',
+    preset: '@shelf/jest-mongodb',
+    transform: {
+      '.+\\.ts$': 'ts-jest'
+    }
+  };
+  
+  
