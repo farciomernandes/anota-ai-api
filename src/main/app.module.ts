07ae36c9db1d1063a58f1b355d8ff2ce0a586360
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-28 12:47:01 -0300

    refactor: remove default files nestJs and create folder main

---
+++ src/main/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CategoryModule } from '../infra/ioc/category-module';
+import { CategoryController } from '../presentation/controllers/category/category-controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      envFilePath: '.env',
+    }),
+    CategoryModule,
+  ],
+  controllers: [CategoryController],
+  providers: [],
+})
+export class AppModule {}
