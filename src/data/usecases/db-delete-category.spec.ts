11fdac5516b88b12316c2db68b765815ed12689b
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-30 17:04:47 -0300

    test: ensure IDbDeleteCategoryRepository

---
+++ src/data/usecases/db-delete-category.spec.ts
@@ -0,0 +1,29 @@
+import { CategoryMongoRepository } from '../../infra/db/mongodb/category/category-mongo-repository';
+import {
+  makeCategoryMongoRepository,
+  makeFakeCategory,
+} from './db-mock-helper-category';
+import { DbDeleteCategory } from './db-delete-category';
+
+interface SutTypes {
+  sut: DbDeleteCategory;
+  deleteCategoryRepositoryStub: CategoryMongoRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const deleteCategoryRepositoryStub = makeCategoryMongoRepository();
+  const sut = new DbDeleteCategory(deleteCategoryRepositoryStub);
+
+  return {
+    sut,
+    deleteCategoryRepositoryStub,
+  };
+};
+describe('DbDeleteCategory usecase', () => {
+  test('Should call CategoryMongoRepository with correct values', async () => {
+    const { sut, deleteCategoryRepositoryStub } = makeSut();
+    const deleteSpy = jest.spyOn(deleteCategoryRepositoryStub, 'delete');
+    await sut.delete(makeFakeCategory().id);
+    expect(deleteSpy).toBeCalledWith(makeFakeCategory().id);
+  });
+});
