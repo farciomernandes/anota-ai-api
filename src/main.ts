369b68085873a7da847f660c9ad206249b24e215
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-30 17:08:14 -0300

    chore: ensure ValidationPipe use validation in Dto

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MongoHelper } from './infra/db/mongodb/helpers/mongo-helper';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   MongoHelper.connect('mongodb://localhost:27017')
     .then(async () => {
       const app = await NestFactory.create(AppModule);
+      app.useGlobalPipes(new ValidationPipe());
+
       await app.listen(3000);
     })
     .catch((err) => console.log(err));
